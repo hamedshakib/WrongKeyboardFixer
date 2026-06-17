499ea5b52bf7e77990939bc8ede2223e9d47ca4c
hamed shakib <hamedshakib1@yahoo.com>
2026-06-17 23:22:16 +0330

    Add project files.

---
+++ WrongKeyboardFixer/MainForm.Designer.cs
@@ -0,0 +1,23 @@
+﻿namespace PersianKeyboardFixer;
+
+partial class MainForm
+{
+    private System.ComponentModel.IContainer? components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+            components.Dispose();
+
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(100, 100);
+        Text = "PersianKeyboardFixer";
+    }
+}
